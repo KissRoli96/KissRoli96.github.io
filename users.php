ddf10ee40596959eaa1ef6a803c6cff1d74ad178
KissRoland <kissrolandkristof@gmail.com>
2021-09-02 19:00:16 +0200

    updated the users.php

--- users.php
+++ users.php
@@ -1,1 +1,32 @@
 <?php
+
+function getUsers()
+{
+    return $users = json_decode(file_get_contents(__DIR__ . '/users.json'), true);
+}
+
+function getUserById($id)
+{
+    $users = getUsers();
+    foreach ($users as $user){
+        if ($user['id'] == $id) {
+            return $user;
+        }
+    }
+    return null;
+}
+
+function createUser($data)
+{
+
+}
+
+function updateUser($data, $id)
+{
+
+}
+
+function deleteUser($id)
+{
+
+}
