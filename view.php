be36c40a05777d2f0314756955bdab74741c3554
KissRoland <kissrolandkristof@gmail.com>
2021-09-03 13:27:13 +0200

    refactor the view file

--- view.php
+++ view.php
@@ -6,18 +6,22 @@ if (!isset($_GET['id'])) {
   include 'partials/not_found.php';
   exit;
 }
+
 $userId = $_GET['id'];
 $user = getUserById($userId);
+
 if (!$user) {
     include 'partials/not_found.php';
     exit;
 }
 ?>
+
 <div class="container">
     <div class="card">
         <div class="card-header">
             <h3>View User: <b><?= $user['name'] ?></b> </h3>
         </div>
+
         <div class="car-body">
             <a class="btn btn-secondary" href="update.php?id=<?= $user['id'] ?>">Update</a>
             <form style="display: inline-block" method="post" action="delete.php">
@@ -25,37 +29,34 @@ if (!$user) {
                 <button class="btn btn-danger">Delete</button>
             </form>
         </div>
+
         <table class="table">
             <tbody>
-            <tr>
-                <th>Name</th>
-                <td><?= $user['name'] ?></td>
-            </tr>
-            <tr>
-                <th>Username</th>
-                <td><?= $user['username'] ?></td>
-            </tr>
-            <tr>
-                <th>Email</th>
-                <td><?= $user['email'] ?></td>
-            </tr>
-            <tr>
-                <th>Phone</th>
-                <td><?= $user['phone'] ?></td>
-            </tr>
-            <tr>
-                <th>Website</th>
-                <td>
-                    <a target="_blank" href="http://<?= $user['website'] ?>">
-                        <?= $user['website'] ?>
-                    </a>
-                </td>
-            </tr>
+                <tr>
+                    <th>Name</th>
+                    <td><?= $user['name'] ?></td>
+                </tr>
+                <tr>
+                    <th>Username</th>
+                    <td><?= $user['username'] ?></td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td><?= $user['email'] ?></td>
+                </tr>
+                <tr>
+                    <th>Phone</th>
+                    <td><?= $user['phone'] ?></td>
+                </tr>
+                <tr>
+                    <th>Website</th>
+                    <td>
+                        <a target="_blank" href="http://<?= $user['website'] ?>"><?= $user['website'] ?></a>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </div>
 </div>
-
-
 
 <?php include 'partials/footer.php' ?>
