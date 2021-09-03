b94e1af0059e5319dee1b09eead9271ce423a528
KissRoland <kissrolandkristof@gmail.com>
2021-09-03 13:05:02 +0200

    updated the index

--- index.php
+++ index.php
@@ -1,36 +1,52 @@
 <?php
-require 'users.php';
+require 'users/users.php';
 include 'partials/header.php';
 $users = getUsers();
 ?>
-
-<body>
-<table class="table">
-    <thead>
-    <tr>
-        <th>Name</th>
-        <th>Username</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>Website</th>
-        <th>Actions</th>
-    </tr>
-    </thead>
-    <tbody>
+<div class="container">
+    <p>
+        <a class="btn btn-success" href="create.php">Create new User</a>
+    </p>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
         <?php foreach ($users as $user ): ?>
             <tr>
+                <td>
+                    <?php if (isset($user['extension'])):?>
+                        <img width="60px" src="<?php echo "users/images/${user['id']}.${user['extension']}" ?>" alt="">
+                    <?php endif; ?>
+                </td>
                 <td><?= $user['name'] ?></td>
                 <td><?= $user['username'] ?></td>
                 <td><?= $user['email'] ?></td>
                 <td><?= $user['phone'] ?></td>
-                <td><?= $user['website'] ?></td>
+                <td>
+                    <a target="_blank" href="http://<?= $user['website'] ?>">
+                        <?= $user['website'] ?>
+                    </a>
+                </td>
                 <td>
                     <a href="view.php?id=<?= $user['id'] ?>" class="btn btn-sm btn-outline-info">View</a>
                     <a href="update.php?id=<?= $user['id'] ?>" class="btn btn-sm btn-outline-secondary">Update</a>
-                    <a href="delete.php?id=<?= $user['id'] ?>" class="btn btn-sm btn-outline-danger">Delete</a>
+                    <form method="post" action="delete.php">
+                        <input type="hidden" name="id" value="<?= $user['id'] ?>">
+                        <button class="btn btn-sm btn-outline-danger">Delete</button>
+                    </form>
                 </td>
             </tr>
         <?php endforeach; ?>
-    </tbody>
-</table>
+        </tbody>
+    </table>
+</div>
 <?php include 'partials/footer.php' ?>
