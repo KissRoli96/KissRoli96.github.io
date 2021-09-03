f122b6e421d3a1a36ed8c0b38124e0a64ab9659d
KissRoland <kissrolandkristof@gmail.com>
2021-09-03 13:06:30 +0200

    refractor the view

--- view.php
+++ view.php
@@ -1,6 +1,6 @@
 <?php
 include 'partials/header.php';
-require  __DIR__ . '/users.php';
+require __DIR__ . '/users/users.php';
 
 if (!isset($_GET['id'])) {
   include 'partials/not_found.php';
@@ -13,36 +13,49 @@ if (!$user) {
     exit;
 }
 ?>
-
-<div class="card">
-    <div class="card-header">
-        <h3>View User: <b><?= $user['name'] ?></b> </h3>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>View User: <b><?= $user['name'] ?></b> </h3>
+        </div>
+        <div class="car-body">
+            <a class="btn btn-secondary" href="update.php?id=<?= $user['id'] ?>">Update</a>
+            <form style="display: inline-block" method="post" action="delete.php">
+                <input  type="hidden" name="id" value="<?= $user['id'] ?>">
+                <button class="btn btn-danger">Delete</button>
+            </form>
+        </div>
+        <table class="table">
+            <tbody>
+            <tr>
+                <th>Name</th>
+                <td><?= $user['name'] ?></td>
+            </tr>
+            <tr>
+                <th>Username</th>
+                <td><?= $user['username'] ?></td>
+            </tr>
+            <tr>
+                <th>Email</th>
+                <td><?= $user['email'] ?></td>
+            </tr>
+            <tr>
+                <th>Phone</th>
+                <td><?= $user['phone'] ?></td>
+            </tr>
+            <tr>
+                <th>Website</th>
+                <td>
+                    <a target="_blank" href="http://<?= $user['website'] ?>">
+                        <?= $user['website'] ?>
+                    </a>
+                </td>
+            </tr>
+            </tbody>
+        </table>
     </div>
-    <table class="table">
-        <tbody>
-        <tr>
-            <th>Name</th>
-            <td><?= $user['name'] ?></td>
-        </tr>
-        <tr>
-            <th>Username</th>
-            <td><?= $user['username'] ?></td>
-        </tr>
-        <tr>
-            <th>Email</th>
-            <td><?= $user['email'] ?></td>
-        </tr>
-        <tr>
-            <th>Phone</th>
-            <td><?= $user['phone'] ?></td>
-        </tr>
-        <tr>
-            <th>Website</th>
-            <td><?= $user['website'] ?></td>
-        </tr>
-        </tbody>
-    </table>
 </div>
+
 
 
 <?php include 'partials/footer.php' ?>
